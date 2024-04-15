fe30b784433e7c2e0c1eac7e2232a83f96af5e04
OeildeLynx31 <vincent@top7box.com>
2024-04-15 09:39:55 +0200

    Remove inused test block

--- src/resources/blocks/display.js
+++ src/resources/blocks/display.js
@@ -24,25 +24,6 @@ function register() {
         const code = `ModAPI.displayToChat({msg: \`${TEXT1}\`});`;
         return `${code}\n`;
     })
-    registerBlock(`${categoryPrefix}startsound`, {
-        message0: 'start sound %1',
-        args0: [
-            {
-                "type": "field_input",
-                "name": "SOUND",
-                "text": "https://t.ly/2gHlM",
-                "spellcheck": false
-            },
-        ],
-        previousStatement: null,
-        nextStatement: null,
-        inputsInline: true,
-        colour: categoryColor,
-    }, (block) => {
-        const SOUND = block.getFieldValue('SOUND')
-        const code = `new Audio(\`${SOUND}\`).play();`;
-        return `${code}\n`;
-    })
 }
 
 export default register;
